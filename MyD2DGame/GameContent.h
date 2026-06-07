e14acd8afaa483e624d7c4f6012d39769f56e50b
Bobue <105865851+Bobue@users.noreply.github.com>
2026-06-07 22:08:10 +0900

    Error Fix

---
+++ MyD2DGame/GameContent.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include "Content.h"
+
+class GameContent : public Content
+{
+public:
+	void OnStart(EngineContext& engine) override;
+	void OnUpdate(EngineContext& engine, float deltaTime) override;
+	void OnEnd(EngineContext& engine) override;
+
+
+private:
+	int mainWindowId = -1;
+};
--- MyD2DGame/GameWindow.h
+++ MyD2DGame/GameWindow.h
@@ -1,33 +1,54 @@
 #pragma once
 #include <Windows.h>
+#include <string>
+
+class InputManager;
+
+//윈도우창 설계정보(이거 기반으로 생성할 수 있게)
+struct WindowCreateInfo
+{
+	std::wstring title = L"Default Window";
+
+	int x	   = 100; //x좌표
+	int y	   = 100; //y좌표
+	int width  = 800; //높이
+	int height = 600; //너비
+};
+
 
 
 
 class GameWindow
 {
 public:
-	GameWindow();
-	~GameWindow();
+	GameWindow() = default;
 
 	bool Create(
-		HINSTANCE hInstance,  // 창의 핸들
-		const wchar_t* title, // 창의 이름
-		int x    , int y    , // 창의 좌표
-		int width, int height // 창의 크기
+		HINSTANCE hInstance,		 // 창의 핸들
+		const wchar_t* title,		 // 창의 이름
+		int id,						 // 창의 아이디
+		const WindowCreateInfo& info, // 창 정보 구조체
+		InputManager* inputManager
 	);
-
-	void Show(int nCmdShow);
 	HWND GetHwnd() const;
+	int  GetID()   const;
 
-	void ResizeWindowToMonitorRatio(HWND hwnd, double widthRatio, double heightRaio, double XRatio, double YRatio, bool flag);
+	LRESULT HandleMessage(HWND hwnd,UINT message, WPARAM wParam, LPARAM lParam);
+
+	//void ResizeWindowToMonitorRatio(HWND hwnd, double widthRatio, double heightRaio, double XRatio, double YRatio, bool flag);
+
+
 private:
-	HWND m_hwnd;
+	HWND m_hwnd=nullptr;
 	HINSTANCE m_hInstance;
+	int id = -1;
+	InputManager* inputManager = nullptr;
 
-private:
-	static LRESULT CALLBACK StaticWndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM IParam);
-		   LRESULT			WndProc		 (HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
-
+//
+//private:
+//	static LRESULT CALLBACK StaticWndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM IParam);
+//		   LRESULT			WndProc		 (HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+//
 
 	
 };
